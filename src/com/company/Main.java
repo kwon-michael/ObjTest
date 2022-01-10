5c071b039b9603018c215ed69adc56a2cc448d05
kwon-michael <90662213+kwon-michael@users.noreply.github.com>
2022-01-10 11:58:56 -0500

    test

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -12,33 +12,40 @@ public class Main {
         //System.out.println(scheduleList);
 
         //printing all the elements individually in scheduleList
-//        for(int i =0; i <scheduleList.size(); i++){
-//            Person scheduled = scheduleList.get(i);
-//            System.out.println(scheduled);
+        for(int i =0; i <scheduleList.size(); i++){
+            Person scheduled = scheduleList.get(i);
+            System.out.println(scheduled);
+        }
+//        //finding what chil does on monday
+//        for(int i = 0; i < scheduleList.size(); i++){
+//            if (scheduleList.get(i).day == "Monday" && scheduleList.get(i).name == "chil"){
+//                Person scheduled = scheduleList.get(i);
+//                System.out.println("On Monday, chil " + scheduled.activity + " at " + scheduled.time);
+//            }
 //        }
-        //finding what chil does on monday
-        for(int i = 0; i < scheduleList.size(); i++){
-            if (scheduleList.get(i).day == "Monday" && scheduleList.get(i).name == "chil"){
-                Person scheduled = scheduleList.get(i);
-                System.out.println("chil " + scheduled.activity + " at " + scheduled.time);
-            }
-        }
-
-        //finding what yun does on monday
-        for(int i = 0; i < scheduleList.size(); i++){
-            if (scheduleList.get(i).day == "Monday" && scheduleList.get(i).name == "yun"){
-                Person scheduled = scheduleList.get(i);
-                System.out.println("yun " + scheduled.activity + " at " + scheduled.time);
-            }
-        }
-
-        //when does chil go to pharmacy?
-        for(int i = 0; i < scheduleList.size(); i++){
-            if (scheduleList.get(i).activity == "goes to pharmacy"){
-                Person specificActivity = scheduleList.get(i);
-                System.out.println(specificActivity.name + " " + specificActivity.activity + " at " + specificActivity.time + " on " + specificActivity.day);
-            }
-        }
+//
+//        //finding what yun does on monday
+//        for(int i = 0; i < scheduleList.size(); i++){
+//            if (scheduleList.get(i).day == "Monday" && scheduleList.get(i).name == "yun"){
+//                Person scheduled = scheduleList.get(i);
+//                System.out.println("On Monday, yun " + scheduled.activity + " at " + scheduled.time);
+//            }
+//        }
+//
+//        //when does chil go to pharmacy?
+//        for(int i = 0; i < scheduleList.size(); i++){
+//            if (scheduleList.get(i).activity == "goes to pharmacy"){
+//                Person specificActivity = scheduleList.get(i);
+//                System.out.println(specificActivity.name + " " + specificActivity.activity + " at " + specificActivity.time + " on " + specificActivity.day);
+//            }
+//        }
+//        //finding what chil does on Tuesday
+//        for(int i = 0; i < scheduleList.size(); i++){
+//            if (scheduleList.get(i).day == "Tuesday" && scheduleList.get(i).name == "chil"){
+//                Person scheduled = scheduleList.get(i);
+//                System.out.println("On Tuesday, chil " + scheduled.activity + " at " + scheduled.time);
+//            }
+//        }
 
     }
 
--- src/com/company/Schedule.java
+++ src/com/company/Schedule.java
@@ -24,6 +24,70 @@ public class Schedule {
         personSchedule.add(new Person("yun", "workout", "8pm","Monday"));
         personSchedule.add(new Person("yun", "goes to sleep", "9pm","Monday"));
 
+        //chil's Tuesday
+        personSchedule.add(new Person("chil", "starts work", "9am", "Tuesday"));
+        personSchedule.add(new Person("chil", "has lunch", "12:10pm","Tuesday"));
+        personSchedule.add(new Person("chil", "ends work", "3:30pm","Tuesday"));
+        personSchedule.add(new Person("chil", "goes to pharmacy", "5:40pm","Tuesday"));
+        personSchedule.add(new Person("chil", "eats diner", "7:10pm","Tuesday"));
+        personSchedule.add(new Person("chil", "goes to sleep", "9:30pm","Tuesday"));
+        //yun's Tuesday
+        personSchedule.add(new Person("yun", "starts work", "9am","Tuesday"));
+        personSchedule.add(new Person("yun", "eats lunch", "1pm","Tuesday"));
+        personSchedule.add(new Person("yun", "ends work", "5:55pm","Tuesday"));
+        personSchedule.add(new Person("yun", "eats dinner", "7pm","Tuesday"));
+        personSchedule.add(new Person("yun", "workout", "8:05pm","Tuesday"));
+        personSchedule.add(new Person("yun", "goes to sleep", "9:30pm","Tuesday"));
+
+        //chil's Wednesday
+        personSchedule.add(new Person("chil", "starts work", "9am", "Wednesday"));
+        personSchedule.add(new Person("chil", "has meeting", "10am","Wednesday"));
+        personSchedule.add(new Person("chil", "ends meeting", "11am","Wednesday"));
+        personSchedule.add(new Person("chil", "has lunch", "12pm","Wednesday"));
+        personSchedule.add(new Person("chil", "ends work", "3:30pm","Wednesday"));
+        personSchedule.add(new Person("chil", "goes to pharmacy", "5:45pm","Wednesday"));
+        personSchedule.add(new Person("chil", "eats diner", "7pm","Wednesday"));
+        personSchedule.add(new Person("chil", "goes to sleep", "9pm","Wednesday"));
+        //yun's Wednesday
+        personSchedule.add(new Person("yun", "starts work", "9am","Wednesday"));
+        personSchedule.add(new Person("yun", "eats lunch", "12pm","Wednesday"));
+        personSchedule.add(new Person("yun", "ends work", "6pm","Wednesday"));
+        personSchedule.add(new Person("yun", "eats dinner", "7pm","Wednesday"));
+        personSchedule.add(new Person("yun", "reads book", "8pm","Wednesday"));
+        personSchedule.add(new Person("yun", "goes to sleep", "9pm","Wednesday"));
+
+        //chil's Thursday
+        personSchedule.add(new Person("chil", "starts work", "9am", "Thursday"));
+        personSchedule.add(new Person("chil", "has lunch", "12pm","Thursday"));
+        personSchedule.add(new Person("chil", "ends work", "3:30pm","Thursday"));
+        personSchedule.add(new Person("chil", "goes to pharmacy", "5:50pm","Thursday"));
+        personSchedule.add(new Person("chil", "eats diner", "7pm","Thursday"));
+        personSchedule.add(new Person("chil", "goes to sleep", "9pm","Thursday"));
+        //yun's Thursday
+        personSchedule.add(new Person("yun", "starts work", "9am","Thursday"));
+        personSchedule.add(new Person("yun", "eats lunch", "12pm","Thursday"));
+        personSchedule.add(new Person("yun", "ends work", "6pm","Thursday"));
+        personSchedule.add(new Person("yun", "eats dinner", "7pm","Thursday"));
+        personSchedule.add(new Person("yun", "goes to sleep", "9pm","Thursday"));
+
+        //chil's Friday
+        personSchedule.add(new Person("chil", "starts work", "9am", "Friday"));
+        personSchedule.add(new Person("chil", "has meeting", "10am","Friday"));
+        personSchedule.add(new Person("chil", "ends meeting", "11am","Friday"));
+        personSchedule.add(new Person("chil", "has lunch", "12:45pm","Friday"));
+        personSchedule.add(new Person("chil", "ends work", "3:30pm","Friday"));
+        personSchedule.add(new Person("chil", "goes to pharmacy", "5:55pm","Friday"));
+        personSchedule.add(new Person("chil", "eats diner", "7pm","Friday"));
+        personSchedule.add(new Person("chil", "goes to sleep", "11pm","Friday"));
+        //yun's Friday
+        personSchedule.add(new Person("yun", "starts work", "9am","Friday"));
+        personSchedule.add(new Person("yun", "eats lunch", "12pm","Friday"));
+        personSchedule.add(new Person("yun", "ends work", "6pm","Friday"));
+        personSchedule.add(new Person("yun", "does deliveries", "6:20pm","Friday"));
+        personSchedule.add(new Person("yun", "eats dinner", "7pm","Friday"));
+        personSchedule.add(new Person("yun", "workout", "8pm","Friday"));
+        personSchedule.add(new Person("yun", "goes to sleep", "11pm","Friday"));
+
     }
     public List<Person> getList() {
         return personSchedule;
